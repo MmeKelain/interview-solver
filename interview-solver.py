30636c41eb4fc969e0ad6c10faddd0c3024fa9f1
Katherine Fackrell <kfackrel@email.arizona.edu>
2014-08-12 09:52:22 -0700

    Create interview-solver.py

---
+++ interview-solver.py
@@ -0,0 +1,41 @@
+import webscraper
+
+def window_washing_service(city):
+    ""
+    Returns a lump sum in US dollars that should be charged to wash all of the windows in a given city.
+    Arguments:
+    city -- the (US) city in desperate need of clean windows
+    ""
+    # find city's population and population density
+    # find median income of window washers: iwca.org
+    # find cost of living
+    
+    # method 1: charge per window
+    
+    # method 2: charge per hour
+    
+    # method 3: charge per window area
+
+def spheres_inside_things(sphere, thing):
+    ""
+    Returns the number of spherical objects that can fit inside a thing.
+    Arguments:
+    sphere -- the spherical objects to put in the thing
+    thing -- the thing to be filled with spherical objects
+    ""
+    if sphere == "ping pong ball" and thing = "school bus":
+        return fixed_number
+    
+    
+def overused_linked_list_cycle(linked_list):
+    ""
+    Returns true if a cycle exists, false otherwise.
+    Arguments:
+    linked_list -- a data structure that should never contain a cycle. Seriously, you could replace this function with 'return developer.is_idiot()'
+    ""
+    
+def absurd_word_problem(full_text):
+    ""
+    Arguments:
+    full_text -- the word problem
+    ""
